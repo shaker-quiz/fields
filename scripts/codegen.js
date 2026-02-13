79587ecaf4ecb4a8563b541bfc917825f764b99f
yurkimus <yurkimus@gmail.com>
2026-02-13 14:39:58 +0100

    Adjust codegen, regenerate

--- scripts/codegen.js
+++ scripts/codegen.js
@@ -1,9 +1,10 @@
 import template from './template.js' with { type: 'text' }
 
 import { Contracts } from '@shakerquiz/contracts'
-import { Methods, Mode, Roles, Routes } from '@shakerquiz/utilities'
+import { Mode } from '@shakerquiz/utilities'
+import { $ } from 'bun'
 
-let Forms = [
+let FormDatas = [
   'POST/theme/cover/admin',
   'PUT/theme/cover/admin',
 ]
@@ -12,26 +13,17 @@ let path = x => x.join('/')
 
 let schema = x => path(x).replaceAll('/', '_')
 
-let all = Methods
-  .flatMap(method => Routes.map(route => [method, route]))
-  .flatMap(([method, route]) => Roles.concat(Mode['Unknown']).map(role => [method, route, role]))
+let raw = await $`find ./source/forms -type f -name '*.json'`
 
-let exists = await Promise
-  .all(all.map(x => Bun.file(`./source/forms/${path(x)}.json`).exists()))
-  .then(exists => exists.reduce((set, e, i) => e ? set.add(all[i]) : set, new Set()))
-  .then(Array.from)
+let exists = raw
+  .text()
+  .split('\n')
+  .filter(Boolean)
+  .map(x => x.replace('./source/forms/', '').replace('.json', '').split('/'))
 
 let imports = exists
   .map(x => `import ${schema(x)} from './forms/${path(x)}.json' with { type: 'json' }`)
   .join('\n')
-
-let forms = exists
-  .map(x => `'${path(x)}'`)
-  .join(',\n    ')
-
-let schemas = exists
-  .map(x => schema(x))
-  .join(',\n    ')
 
 let formSchema = exists
   .map(x => `'${path(x)}': ${schema(x)}`)
@@ -41,7 +33,7 @@ let formKind = exists
   .map(x => {
     if (Contracts.includes(path(x)))
       return `'${path(x)}': 'Schema'`
-    else if (Forms.includes(path(x)))
+    else if (FormDatas.includes(path(x)))
       return `'${path(x)}': 'FormData'`
     else
       return `'${path(x)}': '${Mode['Unknown']}'`
@@ -52,8 +44,6 @@ Bun.write(
   './source/index.js',
   template
     .replace('/* imports */', imports)
-    .replace('/* forms */', forms)
-    .replace('/* schemas */', schemas)
     .replace('/* form -> schema */', formSchema)
     .replace('/* form -> kind */', formKind),
 )
--- scripts/template.js
+++ scripts/template.js
@@ -1,17 +1,5 @@
 /* imports */
 
-export const Forms = Object.freeze(
-  /** @type {const} */ ([
-    /* forms */
-  ]),
-)
-
-export const Schemas = Object.freeze(
-  /** @type {const} */ ([
-    /* schemas */
-  ]),
-)
-
 export const FormSchema = Object.freeze(
   /** @type {const} */ ({
     /* form -> schema */
